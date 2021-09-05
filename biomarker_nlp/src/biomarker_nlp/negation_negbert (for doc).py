70939ddd19f5d60ae95fa181ce09826ac1492e19
JunxiaLin <62398565+Damonlin11@users.noreply.github.com>
2021-09-04 23:41:24 -0400

    Delete negation_negbert (for doc).py

--- biomarker_nlp/src/biomarker_nlp/negation_negbert (for doc).py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""
-This is part of Aditya Khandelwal & Suraj Sawant's (2020) NegBERT program, it is necessary to run the negation cue detection (negCue) and negation scope resolution (negScope) models. For more information about the NegBERT program, please see https://github.com/adityak6798/Transformers-For-Negation-and-Speculation. 
-"""
